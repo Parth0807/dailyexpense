4a08454359080cf4bf413cd2380c6c7e7caa74f1
Parth0807 <parthdesai0849@gmail.com>
2021-03-30 22:39:00 +0530

    Parth1

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,6 @@
 <?php
-$con=mysqli_connect("localhost", "root", "tcet", "detsdb");
+$con=mysqli_connect("database-1.ctzgn5vp6mwi.us-east-1.rds.amazonaws.com
+", "admin", "desaiparth", "database-1");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
